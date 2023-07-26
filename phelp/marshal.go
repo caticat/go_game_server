95b62354fe39f8b3c3adf82c703cd7bbe4238111
pan <catifish@163.com>
2023-07-26 15:17:14 +0800

    去除错误日志

--- phelp/marshal.go
+++ phelp/marshal.go
@@ -2,14 +2,11 @@ package phelp
 
 import (
 	"encoding/json"
-
-	"github.com/caticat/go_game_server/plog"
 )
 
 func ToJson[T any](t T) string {
 	b, e := json.Marshal(t)
 	if e != nil {
-		plog.ErrorLn(e)
 		return ""
 	}
 	return string(b)
@@ -18,7 +15,6 @@ func ToJson[T any](t T) string {
 func ToJsonIndent[T any](t T) string {
 	b, e := json.MarshalIndent(t, "", "\t")
 	if e != nil {
-		plog.ErrorLn(e)
 		return ""
 	}
 	return string(b)
